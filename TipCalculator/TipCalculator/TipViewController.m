cca7217e4a313fb97ba835cd521f5008c975cbdd
haiyongw <haiyongw@users.noreply.github.com>
2015-01-31 01:50:26 -0800

    fully working version

---
+++ TipCalculator/TipCalculator/TipViewController.m
@@ -0,0 +1,67 @@
+//
+//  TipViewController.m
+//  TipCalculator
+//
+//  Created by Haiyong Wang on 1/31/15.
+//  Copyright (c) 2015 Haiyong Wang. All rights reserved.
+//
+
+#import "TipViewController.h"
+
+@interface TipViewController ()
+@property (weak, nonatomic) IBOutlet UITextField *billTextField;
+@property (weak, nonatomic) IBOutlet UILabel *tipLabel;
+@property (weak, nonatomic) IBOutlet UILabel *totalLabel;
+@property (weak, nonatomic) IBOutlet UISegmentedControl *tipControl;
+
+- (IBAction)onTap:(id)sender;
+- (void)updateValues;
+
+@end
+
+@implementation TipViewController
+
+- (instancetype)init {
+    self = [super init];
+    if (self) {
+        self.navigationItem.title = @"Tip Calculator";
+    }
+    return self;
+}
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    // Do any additional setup after loading the view from its nib.
+    [self updateValues];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+- (IBAction)onTap:(id)sender {
+    [self.view endEditing:YES];
+    [self updateValues];
+}
+
+- (void)updateValues {
+    float billAmount = [self.billTextField.text floatValue];
+    NSArray *tipValues = @[@(0.1), @(0.15), @(0.20)];
+    float tipAmount = billAmount * [tipValues[self.tipControl.selectedSegmentIndex]floatValue];
+    float totalAmount = billAmount + tipAmount;
+    self.tipLabel.text = [NSString stringWithFormat:@"$%0.2f", tipAmount];
+    self.totalLabel.text = [NSString stringWithFormat:@"$%0.2f", totalAmount];
+}
+@end
---
+++ TipCalculator/TipCalculator/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TipCalculator
+//
+//  Created by Haiyong Wang on 1/30/15.
+//  Copyright (c) 2015 Haiyong Wang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
